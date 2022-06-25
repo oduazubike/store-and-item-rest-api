8ee44cc38cd69c6c91a5a0168f2fce19becff296
oduazubike <oduazubike@gmail.com>
2022-06-25 12:57:06 +0000

    New trial for postgrresql string

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -8,9 +9,14 @@ from resources.items_resource import Item, ItemsList
 from resources.user_resource import UserRegister, UserLogin
 from resources.stores_resource import Store, StoresList
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(uri, 'sqlite:///database.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "super-secret"
 api = Api(app)
